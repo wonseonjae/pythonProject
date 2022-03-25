6e269853df67d3459548cbdace4bdb8bf4e4249c
wonsunjae <bij7750@gmail.com>
2022-03-25 09:57:18 +0900

    완주하지 못한 선수 문제

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        def solution(participant, completion):
+
+            tmp = 0
+
+            dic = {}
+
+            for p in participant:
+
+                dic[hash(p)] = p
+
+                tmp += int(hash(p))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+            for com in completion:
 
+                    tmp -= hash(com)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            return dic[tmp]
